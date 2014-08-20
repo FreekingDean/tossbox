3844ff9721ea65d1926c59a20a95859a382ab911
Dean <deanfreekinggalvin@gmail.com>
2014-08-20 16:16:58 -0400

    made some shit
    Signed-off-by: Dean <deanfreekinggalvin@gmail.com>

--- commentTest.go
+++ commentTest.go
@@ -10,22 +10,22 @@ import (
 func main() {
   r, err := reddit.NewLoginSession("FuckTheCowboysBot", "im1coolguy", "FuckTheCowboysBot/v0.2 /u/bananaboydean /u/harkins")
   if err != nil { fmt.Println(err) }
-  r_eagles, err := r.AboutSubreddit("eagles")
+  bt, err := r.AboutSubreddit("eagles")
   if err != nil { fmt.Println(err) }
   last := ""
   body := ""
   for {
-    comments, err := r_eagles.Comments(&r.Session, 5, "", last)
+    comments, err := bt.Comments(&r.Session, 5, "", last)
     if err != nil { fmt.Println(err) }
     if len(comments) > 0 {
       last = comments[0].FullID
     }
     for _, comment := range comments {
-      fmt.Println(comment.Body)
       body = strings.ToLower(comment.Body)
-      if strings.Contains(body, "cowboys") {
-        r.Reply(comment, "Fuck the cowboys")
+      if strings.Contains(body, "cowboys") && !strings.Contains(comment.Author, "FuckTheCowboysBot") {
         fmt.Println(comment.Body)
+        fmt.Println(comment.Author)
+        r.Reply(comment, "Fuck the Cowboys")
         fmt.Println("Replied")
       }
     }
